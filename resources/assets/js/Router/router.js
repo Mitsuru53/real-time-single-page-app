390b0bdcc31c7415a9d9435d966e5f9ccdf466a2
Mitsuru Hashimoto <exhikkii@gmail.com>
2018-03-30 18:49:45 +0900

    Login Component and login

--- resources/assets/js/Router/router.js
+++ resources/assets/js/Router/router.js
@@ -2,13 +2,15 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
-
+import Login from '../components/login/Login'
 const routes = [
-{ path: '/login', component: Login }
+  { path: '/login', component: Login }
 ];
 
-const router = new VueRoueter({
-  routes
+const router = new VueRouter({
+  routes,
+  hashbang: false,
+  mode: 'history'
 })
 
 export default router;
--- resources/assets/js/components/AppHome.vue
+++ resources/assets/js/components/AppHome.vue
@@ -1,6 +1,7 @@
 <template lang="html">
   <div class="">
     <toolbar></toolbar>
+    <router-view></router-view>
     <app-footer></app-footer>
   </div>
 </template>
@@ -9,9 +10,10 @@
 
 import toolbar from './Toolbar.vue';
 import AppFooter from './AppFooter.vue';
+import Login from './login/Login.vue';
 
 export default {
-  components: {toolbar, AppFooter}
+  components: {toolbar, AppFooter,Login}
 }
 </script>
 
